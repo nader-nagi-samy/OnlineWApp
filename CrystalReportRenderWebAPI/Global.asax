a285b8a1198d2d3982ccaae8ee409929a09d5248
DELL <DELL@NADERPC>
2023-03-04 02:16:43 +0200

    Add project files.

---
+++ CrystalReportRenderWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrystalReportRenderWebAPI.WebApiApplication" Language="C#" %>
---
+++ CrystalReportRenderWebAPI/Utility/CrystalReport.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Data;
+
+namespace CrystalReportRenderWebAPI.Utility
+{
+    public static class CrystalReport
+    {
+        public static HttpResponseMessage RenderReport(DataTable dataTable, string reportPath, string reportFileName, string exportFilename)
+        {
+            var rd = new ReportDocument();
+
+            rd.Load(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(reportPath), reportFileName));
+            rd.SetDataSource(dataTable);
+            MemoryStream ms = new MemoryStream();
+            using (var stream = rd.ExportToStream(ExportFormatType.WordForWindows))
+            {
+                stream.CopyTo(ms);
+            }
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(ms.ToArray())
+            };
+            result.Content.Headers.ContentDisposition =
+                new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = exportFilename
+                };
+            result.Content.Headers.ContentType =
+                new System.Net.Http.Headers.MediaTypeHeaderValue("application/doc");
+            return result;
+        }
+    }
+}
